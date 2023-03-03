57117a0f403b742f88bb2c67b29267ac10d817cf
GWS-ggg <1356957408@qq.com>
2023-03-03 22:05:20 +0800

    基础首页搭建

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app">
+    <RouterView />
+    <tabBar></tabBar>
+  </div>
+</template>
+
+<script setup>
+import tabBar from "./components/tab-bar/tab-bar.vue";
+</script>
+
+<style lang="less" scoped>
+</style>
