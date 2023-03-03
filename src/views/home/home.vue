3424c1cff3c57a4cb9ec32c63e43801935c004dc
GWS-ggg <1356957408@qq.com>
2023-03-03 23:54:35 +0800

    banner

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,10 +1,53 @@
 <template>
   <div class="home"></div>
   <homeNavBar></homeNavBar>
+  <div class="banner">
+    <img src="@/assets/img/home/banner.webp" alt="" />
+  </div>
+  <div class="location">
+    <div class="city">广州</div>
+    <div class="position">
+      <span class="text">我的位置</span>
+      <img src="@/assets/img/home/icon_location.png" alt="" />
+    </div>
+  </div>
 </template>
 
 <script setup>
 import homeNavBar from "./cpns/home-nav-bar.vue";
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.banner {
+  img {
+    width: 100%;
+  }
+}
+
+.location {
+  display: flex;
+  align-items: center;
+  height: 44px;
+  padding: 0 20px;
+
+  .city {
+    flex: 1;
+  }
+
+  .text {
+    font-size: 12px;
+    position: relative;
+    top: 2px;
+  }
+  .position {
+    width: 74px;
+    display: flex;
+    align-items: center;
+  }
+  img {
+    margin-left: 5px;
+    width: 18px;
+    height: 18px;
+  }
+}
+</style>
