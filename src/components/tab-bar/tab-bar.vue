3a05c293db103b0788bad0f60ba4b65a3330467a
GWS-ggg <1356957408@qq.com>
2023-03-03 23:30:54 +0800

    tabBar

--- src/components/tab-bar/tab-bar.vue
+++ src/components/tab-bar/tab-bar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="tab-bar">
-    <div class="tab-bar-item">
+    <!-- <div class="tab-bar-item">
       <img src="@/assets/img/tabbar/tab_home.png" alt="" />
       <span class="text">首页</span>
     </div>
@@ -15,11 +15,75 @@
     <div class="tab-bar-item">
       <img src="@/assets/img/tabbar/tab_message.png" alt="" />
       <span class="text">消息</span>
-    </div>
+    </div> -->
+
+    <!-- <template v-for="(item, index) in tabbarData">
+      <div
+        class="tab-bar-item"
+        :class="{ active: currentIndex === index }"
+        @click="itmeClick(index, item)"
+      >
+        <img :src="getAssetURL(item.image)" alt="" />
+        <span class="text">{{ item.text }}</span>
+      </div>
+    </template> -->
+
+    <van-tabbar v-model="currentIndex" active-color="#ff9854">
+      <template v-for="(item, index) in tabbarData">
+        <van-tabbar-item :to="item.path">
+          <span>{{ item.text }}</span>
+          <template #icon="props">
+            <img
+              v-if="currentIndex !== index"
+              :src="getAssetURL(item.image)"
+              alt=""
+            />
+            <img v-else :src="getAssetURL(item.imageActive)" alt="" />
+          </template>
+        </van-tabbar-item>
+      </template>
+    </van-tabbar>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { getAssetURL } from "@/utils/load_assets.js";
+import { ref } from "vue";
+import router from "@/router";
+
+const currentIndex = ref(0);
+const itmeClick = (index, item) => {
+  currentIndex.value = index;
+  router.push(item.path);
+};
+
+const tabbarData = [
+  {
+    text: "首页",
+    image: "tabbar/tab_home.png",
+    imageActive: "tabbar/tab_home_active.png",
+    path: "/home",
+  },
+  {
+    text: "收藏",
+    image: "tabbar/tab_favor.png",
+    imageActive: "tabbar/tab_favor_active.png",
+    path: "/favor",
+  },
+  {
+    text: "订单",
+    image: "tabbar/tab_order.png",
+    imageActive: "tabbar/tab_order_active.png",
+    path: "/order",
+  },
+  {
+    text: "消息",
+    image: "tabbar/tab_message.png",
+    imageActive: "tabbar/tab_message.png",
+    path: "/message",
+  },
+];
+</script>
 
 <style lang="less" scoped>
 .tab-bar {
@@ -38,6 +102,10 @@
   justify-content: center;
   flex-direction: column;
   align-items: center;
+
+  &.active {
+    color: var(--primary-color);
+  }
 }
 
 .text {
@@ -46,6 +114,10 @@
 }
 
 img {
-  width: 32px;
+  width: 26px;
+}
+
+:deep(.van-tabbar-item__icon img) {
+  font-size: 32px;
 }
 </style>
