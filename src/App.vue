1f9258fb6132f8e7c1d92d87b9f708ff8b30b8d6
GWS-ggg <1356957408@qq.com>
2023-03-04 12:17:33 +0800

    城市跳转+tabBar隐藏（两种）

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="app">
     <RouterView />
-    <tabBar></tabBar>
+    <tabBar v-if="!router.meta.hideTabBar"></tabBar>
   </div>
 </template>
 
 <script setup>
+import { useRoute } from "vue-router";
 import tabBar from "./components/tab-bar/tab-bar.vue";
+
+const router = useRoute();
 </script>
 
-<style lang="less" scoped>
-</style>
+<style lang="less" scoped></style>
