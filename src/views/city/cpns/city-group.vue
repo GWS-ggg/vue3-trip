38bb923099f5151443f79894453f268d91cb5e41
GWS-ggg <1356957408@qq.com>
2023-03-04 15:52:11 +0800

    热门城市+右侧城市列表

--- src/views/city/cpns/city-group.vue
+++ src/views/city/cpns/city-group.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="city-group">
-    <van-index-bar>
-      <template v-for="(group, index) in groupData?.cities" :key="index">
+    <van-index-bar :index-list="indexList">
+      <van-index-anchor index="热门" />
+      <div class="list">
+        <!-- <template v-for="(item,index) in groupData.hotCities" > -->
+        <template v-for="(item, index) in groupData.hotCities">
+          <div class="city">{{ item.cityName }}</div>
+        </template>
+      </div>
+      <template v-for="(group, index) in groupData.cities" :key="index">
         <van-index-anchor :index="group.group" />
         <template v-for="(city, indey) in group.cities" :key="indey">
           <van-cell :title="city.cityName" />
@@ -12,12 +19,37 @@
 </template>
 
 <script setup>
-defineProps({
+const props = defineProps({
   groupData: {
     type: Object,
     default: () => ({}),
   },
 });
+const indexList = computed(() => {
+  let list = props.groupData.cities.map((item) => item.group);
+  list.unshift("#");
+  return list;
+});
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  padding: 10px;
+  padding-right: 25px;
+}
+
+.city {
+  width: 70px;
+  height: 28px;
+  border-radius: 14px;
+  font-size: 12px;
+  color: #000;
+  line-height: 28px;
+  text-align: center;
+  background-color: #fff4ec;
+  margin: 6px 0;
+}
+</style>
