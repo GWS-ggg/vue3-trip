d4b0b0b1e82a01cda9cfb9fb990b0c520300ce54
GWS-ggg <1356957408@qq.com>
2023-03-04 12:56:29 +0800

    搜索框

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="city">
-    <h2>city</h2>
+    <van-search
+      v-model="searchValue"
+      shape="round"
+      show-action
+      placeholder="城市/区域/位置"
+      @cancel="cancelClick"
+    />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+
+const router = useRouter();
+const cancelClick = () => {
+  router.back();
+};
+const searchValue = ref("");
+</script>
 
 <style lang="less" scoped></style>
